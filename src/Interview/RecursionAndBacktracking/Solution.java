21e6db934b13936099cb065de6695775b76cf7e0
Rakesh Kumar <rkrakeshkumar771996@gmail.com>
2020-12-27 22:41:28 +0530

    First

---
+++ src/Interview/RecursionAndBacktracking/Solution.java
@@ -0,0 +1,26 @@
+package Interview.RecursionAndBacktracking;
+
+import java.util.*;
+
+public class Solution {
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int sum = fibonacci(n - 1) + fibonacci(n - 2);
+        return sum;
+        // Complete the function.
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
+    }
+}
