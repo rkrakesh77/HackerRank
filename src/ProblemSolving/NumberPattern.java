fe3ac1b329499b4d7463c5d9fe4b5a98d8bf7d05
Rakesh Kumar <rkrakeshkumar771996@gmail.com>
2021-01-26 20:15:52 +0530

    Added

--- src/ProblemSolving/NumberPattern.java
+++ src/ProblemSolving/NumberPattern.java
@@ -1,8 +1,4 @@
 package ProblemSolving;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class NumberPattern {
---
+++ src/ProblemSolving/OperationsOnString.java
@@ -0,0 +1,34 @@
+package ProblemSolving;
+
+import java.util.Scanner;
+
+public class OperationsOnString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+//       A=And Operation B=OR Operation C=XOR Operation
+        System.out.println(new OperationsOnString().BitOperationOnString(input));
+    }
+
+    public int BitOperationOnString(String s) {
+        int r = 0;
+        String sign = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0) {
+                r = Integer.parseInt(String.valueOf(s.charAt(i)));
+            } else if (String.valueOf(s.charAt(i)).matches("[0-9]")) {
+                int x = Integer.parseInt(String.valueOf(s.charAt(i)));
+                if (sign.equals("A")) {
+                    r = r & x;
+                } else if (sign.equals("B")) {
+                    r = r | x;
+                } else if (sign.equals("C")) {
+                    r = r ^ x;
+                }
+            } else {
+                sign = String.valueOf(s.charAt(i));
+            }
+        }
+        return r;
+    }
+}
